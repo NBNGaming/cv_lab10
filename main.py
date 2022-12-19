319b2f36bc0dba0fec6cfe0a7d8360d5e97f73e3
Николай Батычко <nbngamings@gmail.com>
2022-12-19 12:38:16 +0300

    Добавил поиск по тексту

--- main.py
+++ main.py
@@ -18,22 +18,31 @@ def load_db():
 @st.cache(allow_output_mutation=True)
 def load_model():
     clip, _, preproc = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion400m_e32')
-    return clip, preproc
+    text_tokenizer = open_clip.get_tokenizer('ViT-B-32')
+    return clip, preproc, text_tokenizer
 
 
 db, neighbours = load_db()
-model, preprocess = load_model()
+model, preprocess, tokenizer = load_model()
 st.title('Image Search')
 uploaded_file = st.file_uploader('Choose an image...', type=['png', 'jpg', 'jpeg', 'webp', 'tiff'])
+st.caption('or')
+text = st.text_input('Enter a text query...').strip()
 
+vec = None
 if uploaded_file is not None:
     img = Image.open(uploaded_file).convert('RGB')
     img = ImageOps.exif_transpose(img)
     with torch.no_grad():
         image = preprocess(img).unsqueeze(0)
         vec = model.encode_image(image).cpu().detach().numpy()
+elif text != '':
+    with torch.no_grad():
+        tokens = tokenizer(text)
+        vec = model.encode_text(tokens).cpu().detach().numpy()
 
-    indices = neighbours.kneighbors(vec.reshape(1, -1), return_distance=False)[0]
+if vec is not None:
+    indices = neighbours.kneighbors(vec, return_distance=False)[0]
     paths = np.hstack(db.loc[indices, ['path']].values)
 
     for path in paths:
